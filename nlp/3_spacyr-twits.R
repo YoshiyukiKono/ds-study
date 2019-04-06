06b95e56448165b276d06ab98dcea3a1b52907b4
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2019-04-06 13:14:04 +0900

    Update and rename spacyr-twits.R to 3_spacyr-twits.R

--- nlp/3_spacyr-twits.R
+++ nlp/3_spacyr-twits.R
@@ -1,3 +1,10 @@
+# Step 3: Hiveテーブルのデータを分析する。
+#
+# 1. SparkでHiveテーブルのデータを読む。(Spacyrで利用できるTidy Data Frame形式)
+# 2. Sentiment単位、Ticker単位等で集計する。
+# 3. Text Bodyの内容をSpacerで自然言語処理を行い、傾向を観察する
+#
+#
 ## spacyr example with stats
 # This example aggregates sentiment flags from twits messages.
 # [spacyr](https://github.com/kbenoit/spacyr) is R binding of [SpaCy](https://spacy.io/), which is Python library for NLP.
